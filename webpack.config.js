f01beccc304a53d86ab0be7944fc309503f7b8e4
jayjamesjay <jakubjezewski@outlook.com>
2019-06-22 14:09:24 +0200

    fix webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './src/components/index.jsx',
+    main: './src/components/Index.jsx',
     worker: './src/service-worker.js'
   },
   output: {
